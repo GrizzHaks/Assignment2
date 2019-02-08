0e5f7e3c2539977606f0910a6094635cf786c91e
GrizzHaks <cpaiello@oakland.edu>
2019-02-07 21:28:38 -0500

    beerSong skeleton

--- src/BeerSong.java
+++ src/BeerSong.java
@@ -1,7 +1,11 @@
-public class Main {
+public class BeerSong {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+    }
+
+    public static void Ninety_Nine_Bottles_Of_Beer(){
+
     }
 
 }
